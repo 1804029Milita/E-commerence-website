9ef9b0986399769dbd979e11056e7f3a903c8cee
1804029Milita <1804029nishat@gmail.com>
2024-11-10 03:18:54 +0600

    update

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const bar= document.getElementById('bar');
-const nav= document.getElementById('nav');
+const bar = document.getElementById('bar');
+const nav = document.getElementById('nav');
+const close= document.getElementById('close');
 
 if (bar) {
-    bar.addEventListener('click', ()=>{
+    console.log("Bar element found"); // Debugging line
+    bar.addEventListener('click', () => {
+        console.log("Bar clicked"); // Debugging line
         nav.classList.add('active');
+    });
+} else {
+    console.log("Bar element not found"); // Debugging line
+}
+if (close) {
+    close.addEventListener('click', () =>{
+        nav.classList.remove('active');
     })
     
 }
